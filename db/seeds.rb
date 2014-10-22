d16f77191cac4e28a2bb80ce6434299ad1c297d0
Brice Sanchez <brice@bisscomm.com>
2014-10-22 17:00:41 -0400

    Add page seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,28 @@
-Refinery::I18n.frontend_locales.each do |lang|
-  I18n.locale = lang
+Refinery::User.find_each do |user|
+  user.plugins.where(name: 'refinerycms-retailers').first_or_create!(
+    position: (user.plugins.maximum(:position) || -1) +1
+  )
+end if defined?(Refinery::User)
 
-  Refinery::User.find_each do |user|
-    user.plugins.where(name: 'refinerycms-retailers').first_or_create!(
-      position: (user.plugins.maximum(:position) || -1) +1
-    )
-  end if defined?(Refinery::User)
+if defined?(::Refinery::Page)
+  retailers_index_page = Refinery::Page.find_by(:link_url => Refinery::Retailers.page_path_index)
 
-  Refinery::Page.where(link_url: (url = "/retailers")).first_or_create!(
-    title: 'Retailers',
-    deletable: false,
-    menu_match: "^#{url}(\/|\/.+?|)$"
-  ) do |page|
-    Refinery::Pages.default_parts.each_with_index do |part, index|
-      page.parts.build title: part, body: nil, position: index
-    end
-  end if defined?(Refinery::Page)
+  unless retailers_index_page
+    retailers_index_page = ::Refinery::Page.create({
+      :title => "Retailers",
+      :link_url => Refinery::Retailers.page_path_index,
+      :menu_match => "^(/retailers.*)|#{Refinery::Retailers.page_path_index}$",
+      :deletable => false
+    })
+    retailers_index_page.parts.create({
+      :title => "Body",
+      :body => "<p>Contact our retailers.</p>",
+      :position => 0
+    })
+    retailers_index_page.parts.create({
+      :title => "Side Body",
+      :body => "",
+      :position => 1
+    })
+  end
 end

95edd0f731b17aaa02fce554d44e38bd582298dc
Brice Sanchez <brice@bisscomm.com>
2014-10-22 15:22:26 -0400

    permit retailers params

--- app/controllers/refinery/retailers/admin/retailers_controller.rb
+++ app/controllers/refinery/retailers/admin/retailers_controller.rb
@@ -9,6 +9,13 @@ module Refinery
         def subregion_options
           render partial: 'subregion_select'
         end
+
+        private
+
+          def retailer_params
+            params.require(:retailer).permit(:title, :contact, :address, :country_code,
+              :state_code, :zipcode, :phone, :fax, :email, :website, :draft)
+          end
       end
     end
   end

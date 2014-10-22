112608bfe9517bf46246e37f62592bc27c8a5cea
Brice Sanchez <brice@bisscomm.com>
2014-10-22 16:59:56 -0400

    Add page_path_index config

--- lib/refinery/retailers/configuration.rb
+++ lib/refinery/retailers/configuration.rb
@@ -2,8 +2,9 @@ module Refinery
   module Retailers
     include ActiveSupport::Configurable
 
-    config_accessor :country_priority
+    config_accessor :country_priority, :page_path_index
 
     self.country_priority = %w(US CA)
+    self.page_path_index = "/retailers"
   end
 end

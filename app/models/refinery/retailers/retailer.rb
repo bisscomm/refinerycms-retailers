f3d653222772713c5a07eb194d5366f643d868da
Brice Sanchez <brice@bisscomm.com>
2014-10-22 15:58:41 -0400

    Change validates from title to address

--- app/models/refinery/retailers/retailer.rb
+++ app/models/refinery/retailers/retailer.rb
@@ -3,8 +3,7 @@ module Refinery
     class Retailer < Refinery::Core::BaseModel
       self.table_name = 'refinery_retailers'
 
-
-      validates :title, :presence => true, :uniqueness => true
+      validates :address, :presence => true, :uniqueness => true
 
       acts_as_indexed :fields => [:title, :contact, :address, :country_code, :state_code, :city]
     end

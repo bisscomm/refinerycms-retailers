42b1ffb958906fb9b977186248d1ee4c9d7b23b2
Brice Sanchez <brice@bisscomm.com>
2014-10-23 11:01:49 -0400

    Add method to add http on website url

--- app/models/refinery/retailers/retailer.rb
+++ app/models/refinery/retailers/retailer.rb
@@ -3,11 +3,21 @@ module Refinery
     class Retailer < Refinery::Core::BaseModel
       self.table_name = 'refinery_retailers'
 
+      before_validation :smart_add_url_protocol
+
       validates :address, :presence => true, :uniqueness => true
 
       acts_as_indexed :fields => [:title, :contact, :address, :country_code, :state_code, :city]
 
       scope :published, -> { where :draft => false }
+
+      protected
+
+        def smart_add_url_protocol
+          unless self.website[/\Ahttp:\/\//] || self.website[/\Ahttps:\/\//]
+            self.website = "http://#{self.website}"
+          end
+        end
     end
   end
 end

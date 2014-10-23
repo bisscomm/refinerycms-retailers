1dee36e35b2e9a018725058630101e161eb70644
Brice Sanchez <brice@bisscomm.com>
2014-10-23 11:27:41 -0400

    Group retailers by countries

--- app/controllers/refinery/retailers/retailers_controller.rb
+++ app/controllers/refinery/retailers/retailers_controller.rb
@@ -6,6 +6,9 @@ module Refinery
       before_action :find_all_retailers
       before_action :find_page, only: :index
 
+      def index
+        @retailer_countries = @retailers.group_by { |t| t.country_code }
+      end
     end
   end
 end
